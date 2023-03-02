c5980da8c03c7502dd08f1d09a94c63dbbfdb90c
NITHISH0403 <nithish.kumar@tringapps.com>
2023-03-02 18:40:08 +0530

    upadated

--- src/main/java/org/example/Package/CreditCard.java
+++ src/main/java/org/example/Package/CreditCard.java
@@ -5,6 +5,8 @@ import java.util.logging.*;
 public class  CreditCard
 {
         Logger l = Logger.getLogger("com.api.jar");
+        String holdername = "Holder Name:";
+        String cardno = "Card No:";
 
         public void credit() {
             Card o1 = new Card();
@@ -12,15 +14,15 @@ public class  CreditCard
             //clone o1.
             Card o2 = (Card) o1.clone();
 
-            l.log(Level.INFO, () -> "Holder Name:" + o1.hname + " " + "Card No:" + o1.cno + " " + "Valid To:" + o1.vto);
+            l.log(Level.INFO, () -> holdername + o1.hname + " " + cardno + o1.cno + " " + "Valid To:" + o1.vto);
             if (o1.hname.equals(o2.holderName) && o1.cno.equals(o2.cardNo) && o1.vto.equals(o2.validTo)) {
                 l.info("Your Co-ordinates is True.");
-                l.log(Level.INFO, () -> "Holder Name:" + o1.hname + " " + " Card NO: " + "y1:" + o1.cno);
-                l.log(Level.INFO, () -> "Holder Name:" + o1.holderName + " " + " Card NO: " + o1.cardNo);
+                l.log(Level.INFO, () -> holdername + o1.hname + " " + cardno + "y1:" + o1.cno);
+                l.log(Level.INFO, () -> holdername + o1.holderName + " " + cardno + o1.cardNo);
             } else {
                 l.info("Your Co-ordinates is False.");
-                l.log(Level.INFO, () -> "Holder Name:" + o1.holderName + " " + " Card NO: " + o1.cardNo);
-                l.log(Level.INFO, () -> "Holder Name:" + o1.hname + " " + " Card NO: " + o1.cno);
+                l.log(Level.INFO, () -> holdername + o1.holderName + " " + cardno + o1.cardNo);
+                l.log(Level.INFO, () -> holdername + o1.hname + " " + cardno + o1.cno);
             }
         }
 }
