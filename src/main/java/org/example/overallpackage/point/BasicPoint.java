20383831c6026e2a58465a43c669de5f509ad098
NITHISH0403 <nithish.kumar@tringapps.com>
2023-03-03 09:57:26 +0530

    upadated

--- src/main/java/org/example/overallpackage/point/BasicPoint.java
+++ src/main/java/org/example/overallpackage/point/BasicPoint.java
@@ -16,7 +16,7 @@ public class BasicPoint {
             if (o1.x1.equals(o2.x2) && o1.y1.equals(o2.y2)) {
                 l.log(Level.INFO,()->"Your Co-ordinates is True.");
                 l.log(Level.INFO,()->"x1:" + o1.x1 + " " + "y1:" + o1.y1);
-                l.info("x2:" + o1.x2 + " " + "y2:" + o1.y2);
+                l.log(Level.INFO,()->"x2:" + o1.x2 + " " + "y2:" + o1.y2);
             } else {
                 l.log(Level.INFO,()->"Your Co-ordinates is False.");
                 l.log(Level.INFO,()->"x1:" + o1.x1 + " " + "y1:" + o1.y1);
