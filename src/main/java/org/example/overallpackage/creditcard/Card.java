8702b326e5cbdb439d1d17db3ad43452826d14dc
NITHISH0403 <nithish.kumar@tringapps.com>
2023-03-03 09:42:06 +0530

    upadated

--- src/main/java/org/example/overallpackage/creditcard/Card.java
+++ src/main/java/org/example/overallpackage/creditcard/Card.java
@@ -29,7 +29,7 @@ class Card implements Cloneable
     public Object clonecard(){
         try{
             //call clone in object.
-            return (Card) super.clone();
+            return super.clone();
         }catch (CloneNotSupportedException e){
             l.info("Cloning not allowed");
             return this;
